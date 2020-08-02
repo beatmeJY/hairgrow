1d71e8ab109ea9b355d26a9b3916e2930d49edd4
Choi_JiYoul <wldbf97@naver.com>
2020-08-02 13:26:08 +0900

    Programming Start!

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+      <div>hi</div>
+    </>
+  );
+}
+
+export default App;
